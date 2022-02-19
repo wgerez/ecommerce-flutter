003445ff7ece1f60f0660d4955548981c668ddd6
Walter <wgerez@gmail.com>
2022-02-19 19:36:00 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ECOMMERCE FLUTTER APP
+
+## Description
+
+Ecommerce App with flutter framework
+
+- Flutter framework 2.8.0-stable
+- Clean Architecture
+- SOLID Principles
+- BLOC
---
+++ lib/models/Product.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Product({
+    required this.id,
+    required this.images,
+    required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    required this.title,
+    required this.price,
+    required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/ps4_console_white_1.png",
+      "assets/images/ps4_console_white_2.png",
+      "assets/images/ps4_console_white_3.png",
+      "assets/images/ps4_console_white_4.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Control Wireless para PS4™",
+    price: 64.99,
+    description: description,
+    rating: 4.8,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/Image Popular Product 2.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Nike Blanco - Short Hombre",
+    price: 50.5,
+    description: description,
+    rating: 4.1,
+    isPopular: true,
+  ),
+  Product(
+    id: 3,
+    images: [
+      "assets/images/glap.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Guantes XC Omega - Polígono",
+    price: 36.55,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 4,
+    images: [
+      "assets/images/wireless_headset.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Auricular Logitech",
+    price: 20.20,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+  ),
+];
+
+const String description =
+    "El controlador inalámbrico para PS4™ le brinda lo que desea en sus juegos, desde un control de precisión de sus juegos hasta el uso compartido …";
